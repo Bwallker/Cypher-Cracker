eb51b6da5d8b243fc7b431bcf1cc6b4bc80a32d2
bwallker <benbwall@gmail.com>
2021-10-15 11:19:15 +0300

    - Moved vignere solution into main.py

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
-import time
-
+import typing
 
 def decrypt(cypher_text: str, key: int) -> str:
+    ASCII_VALUE_OF_CAPITAL_A = 65
+    ASCII_VALUE_OF_LOWER_CASE_A = 97
+    SIZE_OF_ALPHABET = 26
     message = ""
     for char in cypher_text:
         if char.isupper():
             ascii_value = ord(char)
-            new_ascii_value = (ascii_value - key - 65) % 26 + 65
+            new_ascii_value = (ascii_value - key - ASCII_VALUE_OF_CAPITAL_A) % SIZE_OF_ALPHABET + ASCII_VALUE_OF_CAPITAL_A
             message += chr(new_ascii_value)
         elif char.islower():
             ascii_value = ord(char)
-            new_ascii_value = (ascii_value - key - 97) % 26 + 97
+            new_ascii_value = (ascii_value - key - ASCII_VALUE_OF_LOWER_CASE_A) % SIZE_OF_ALPHABET + ASCII_VALUE_OF_LOWER_CASE_A
             message += chr(new_ascii_value)
         else:
             message += char
@@ -18,15 +20,18 @@ def decrypt(cypher_text: str, key: int) -> str:
 
 
 def encrypt(message: str, key: int) -> str:
+    ASCII_VALUE_OF_CAPITAL_A = 65
+    ASCII_VALUE_OF_LOWER_CASE_A = 97
+    SIZE_OF_ALPHABET = 26
     cypher_text = ""
     for char in message:
         if char.isupper():
             ascii_value = ord(char)
-            new_ascii_value = (ascii_value + key - 65) % 26 + 65
+            new_ascii_value = (ascii_value + key - ASCII_VALUE_OF_CAPITAL_A) % SIZE_OF_ALPHABET + ASCII_VALUE_OF_CAPITAL_A
             cypher_text += chr(new_ascii_value)
         elif char.islower():
             ascii_value = ord(char)
-            new_ascii_value = (ascii_value + key - 97) % 26 + 97
+            new_ascii_value = (ascii_value + key - ASCII_VALUE_OF_LOWER_CASE_A) % SIZE_OF_ALPHABET + ASCII_VALUE_OF_LOWER_CASE_A
             cypher_text += chr(new_ascii_value)
         else:
             cypher_text += char
@@ -104,3 +109,87 @@ def get_key() -> int:
 
 if __name__ == '__main__':
     main()
+
+# Vignere solution
+def next_char(string: str) -> typing.Generator:
+    i = 0
+    while True:
+        try:
+            yield string[i]
+            i += 1
+        except IndexError:
+            yield string[0]
+            i = 1
+        
+def vignere_encrypt(message: str, key: str) -> str:
+    key = key.replace(' ', '')
+    cypher_text = ""
+    gen = next_char(key)
+    
+    for char in message:
+        if not char.islower() and not char.isupper():
+            cypher_text += char
+            continue
+        key_char_offset_from_a: int
+        key_char: str = next(gen)
+        if key_char.islower():
+            key_char_offset_from_a = ord(key_char) - 97
+        elif key_char.isupper():
+            key_char_offset_from_a = ord(key_char) - 65
+        else:
+            cypher_text += char
+            continue
+        
+        char_offset_from_a: int
+        if char.islower():
+            char_offset_from_a = ord(char) - 97
+        else:
+            char_offset_from_a = ord(char) - 65
+        
+        new_offset = (char_offset_from_a + key_char_offset_from_a) % 26
+        
+        if char.islower():
+            new_char = chr(new_offset + 97)
+        else:
+            new_char = chr(new_offset + 65)
+        
+        cypher_text += new_char
+    
+    
+    return cypher_text
+
+
+def vignere_decrypt(cypher_text: str, key: str) -> str:
+    key = key.replace(' ', '')
+    gen = next_char(key)
+    message = ""
+    for char in cypher_text:
+        if not char.islower() and not char.isupper():
+            message += char
+            continue
+        key_char_offset_from_a: int
+        key_char: str = gen.__next__()
+        if key_char.islower():
+            key_char_offset_from_a = ord(key_char) - 97
+        elif key_char.isupper():
+            key_char_offset_from_a = ord(key_char) - 65
+        else:
+            message += char
+            continue
+
+        char_offset_from_a: int
+        if char.islower():
+            char_offset_from_a = ord(char) - 97
+        else:
+            char_offset_from_a = ord(char) - 65
+
+        new_offset = (char_offset_from_a - key_char_offset_from_a) % 26
+
+        if char.islower():
+            new_char = chr(new_offset + 97)
+        else:
+            new_char = chr(new_offset + 65)
+
+        message += new_char
+
+    return message
